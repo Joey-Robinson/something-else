300be3f45fd66f40712bbc825109e2052a3535a8
Joey Robinson <theamazingness@gmail.com>
2019-03-13 14:01:59 -0400

    Added data.js that contains information about hops

---
+++ src/js/data/hops.js
@@ -0,0 +1,102 @@
+export const hops = [
+  {
+    name: 'Cascade',
+    origin: 'USA',
+    flavors: 'A hint of floral and/or grapefruit.'
+  },
+  {
+    name: 'Centennial',
+    origin: 'USA',
+    flavors: 'Similar to Cascade, expect to find a floral and/or grapefruit flavor from this hop'
+  },
+  {
+    name: 'Columbus or CTZ',
+    origin: 'USA',
+    flavors: 'Pungent and flashy, this hop is often thought of as herbaceous and has notes similar to marijuana and pine resin'
+  },
+  {
+    name: 'Chinook',
+    origin: 'USA',
+    flavors: 'Chinook is often considered a more supportive hop that allows other flavors to come through strong. Expect a citrus flavor with this hop'
+  },
+  {
+    name: 'Willamette',
+    origin: 'USA',
+    flavors: 'This hop produces a mellow earthy spiciness. It also has a much brighter and more citrus flavor than its relative, Fuggle'
+  },
+  {
+    name: 'Citra',
+    origin: 'USA',
+    flavors: 'This hop has a variety of flavors. Ranging from an orangey flavor, this hop can also have other aromas such as mango, passion fruit, pineapple, and peach.'
+  },
+  {
+    name: 'Simcoe',
+    origin: 'USA',
+    flavors: 'Contains flavors of grapefruit, pine, sweet onion, and tropical fruit'
+  },
+  {
+    name: 'Mosaic',
+    origin: 'USA',
+    flavors: 'Often said to have a blueberry flavor profile, this hop can also contain notes of tangerine, pineapple, and peach'
+  },
+  {
+    name: 'Amarillo',
+    origin: 'USA',
+    flavors: 'A classic American hop. This contains a punchy aroma of of orange blossoms'
+  },
+  {
+    name: 'Hallertau',
+    origin: 'Germany',
+    flavors: 'The most famous of all EU hops, this provides a gentle and floral hint with some slight peppery or woodsy spiciness'
+  },
+  {
+    name: 'Tettnanger',
+    origin: 'Germany',
+    flavors: 'Zesty and grassy with an earthy spiciness and a touch of citrus.'
+  },
+  {
+    name: 'Splat',
+    origin: 'Germany',
+    flavors: 'Easy-going, woodsy, and peppery'
+  },
+  {
+    name: 'Saaz',
+    origin: 'Czech',
+    flavors: 'An assertive earthy flavor'
+  },
+  {
+    name: 'Fuggle',
+    origin: 'England',
+    flavors: 'Earthy, cedary, minty, and floral'
+  },
+  {
+    name: 'Challenger',
+    origin: 'England',
+    flavors: 'This particular hop contains notes of tea-like earthiness alongside a lemon marmalade-like fruity bitterness'
+  },
+  {
+    name: 'Golding',
+    origin: 'England',
+    flavors: 'Earthy, pepper, lemon-like character'
+  },
+  {
+    name: 'Northern Brewer',
+    origin: 'England',
+    flavors: 'Woodsy mintiness alongside pine-like aromas'
+  },
+  {
+    name: 'Nelson',
+    origin: 'New Zeland',
+    flavors: 'Lychee, melon, gooseberry flavors'
+  },
+  {
+    name: 'Galaxy',
+    origin: 'Australlia',
+    flavors: 'Contains passion fruit-like juiciness but the flavor can have a pearch or orange tilt as well'
+  },
+  {
+    name: 'Motueka',
+    origin: 'New Zeland',
+    flavors: 'Bring lime-like pop and some tropical fruitiness associated with kiwi'
+  },
+]
