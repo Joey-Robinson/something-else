bd87444f59b33456944d38d0bf0b41d7b62e4c82
Joey Robinson <theamazingness@gmail.com>
2019-03-18 21:06:44 -0400

    God save me

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,10 +2,15 @@ export const ceoModal = () => {
   const ceo = document.querySelector('.ceo__modal--open')
   const modal = document.querySelector('.modal__ceo')
   const modalClose = document.querySelector('.ceo__modal--close')
+  const modalExit = document.querySelector('.exit__modal--ceo')
+
   ceo.addEventListener("click", () => {
     modal.style.display = 'grid'
   })
   modalClose.addEventListener("click", () => {
+    modal.style.display = 'none'
+  })
+  modalExit.addEventListener("click", () => {
     modal.style.display = 'none'
   })
 }
@@ -14,10 +19,15 @@ export const ctoModal = () => {
   const cto = document.querySelector('.cto__modal--open')
   const modal = document.querySelector('.modal__cto')
   const modalClose = document.querySelector('.cto__modal--close')
+  const modalExit = document.querySelector('.exit__modal--cto')
+
   cto.addEventListener("click", () => {
     modal.style.display = 'grid'
   })
   modalClose.addEventListener("click", () => {
+    modal.style.display = 'none'
+  })
+  modalExit.addEventListener("click", () => {
     modal.style.display = 'none'
   })
 }
@@ -26,10 +36,15 @@ export const cfoModal = () => {
   const cfo = document.querySelector('.cfo__modal--open')
   const modal = document.querySelector('.modal__cfo')
   const modalClose = document.querySelector('.cfo__modal--close')
+  const modalExit = document.querySelector('.exit__modal--cfo')
+
   cfo.addEventListener("click", () => {
     modal.style.display = 'grid'
   })
   modalClose.addEventListener("click", () => {
+    modal.style.display = 'none'
+  })
+  modalExit.addEventListener("click", () => {
     modal.style.display = 'none'
   })
 }
